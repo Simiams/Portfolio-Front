fe9cd01b655c12bbce3a74225cd25a65c18612c3
Simiams <simon.convert@epsi.fr>
2023-05-02 15:37:42 +0200

    push pour rendre

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
 import Experience from "./pages/Experience";
